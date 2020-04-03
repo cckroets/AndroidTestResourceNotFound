438fe377635dfe8df9e0aa2c7c27aca302a7731e
ckroetsc <ckroetsch@linkedin.com>
2020-04-03 00:03:58 -0700

    Initial commit. All tests pass

---
+++ android-tests/src/main/java/com/github/cckroets/android/tests/ExampleInstrumentedTest.java
@@ -0,0 +1,30 @@
+package com.github.cckroets.android.tests;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.github.cckroets.mylibrary.R;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Log.d("FromTestModule", "library: hello_from_library: " + Integer.toHexString(R.string.hello_from_library));
+        Assert.assertTrue(appContext.getString(R.string.hello_from_library).contains("Hello"));
+    }
+}
